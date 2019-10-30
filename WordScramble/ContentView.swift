9e861856cc697a5b35cd86e266516cc78ff3055e
sxc <qiuyequ@icloud.com>
2019-10-30 10:33:46 +0800

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Xiaochun Shen on 2019/10/30.
+//  Copyright © 2019 SXC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
